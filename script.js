f3778e6a3dec6e3f4c98edbe2338395bea366a8d
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-09-01 16:40:14 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputs=document.querySelectorAll('.input');
 
+const button=document.querySelector('.login_button');
+
 const handlefocus=({ target })=>{
     const span=target.previousElementSibling;
     span.classList.add('span-active');
@@ -13,5 +15,16 @@ const handlefocusout=({ target })=>{
     }
 }
 
+const handleChange=()=>{
+    const [username,password]=inputs;
+
+    if(username.value && password.value.length>=8){
+        button.removeAttribute('disabled');
+    }else{
+        button.setAttribute('disabled','');
+    }
+}
+
 inputs.forEach((input)=> input.addEventListener('focus',handlefocus));
 inputs.forEach((input)=> input.addEventListener('focusout',handlefocusout));
+inputs.forEach((input)=> input.addEventListener('input',handleChange));
