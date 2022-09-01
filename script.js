64d58f948f44d39dd532254b38ff178069705096
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-09-01 15:27:27 -0300

    Adicoes

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const inputs=document.querySelectorAll('.input');
 
 const handlefocus=({ target })=>{
     const span=target.previousElementSibling;
+    span.classList.add('span-active');
+}
+
+const handlefocusout=({ target })=>{
+    if(target.value==''){
+
+    const span=target.previousElementSibling;
+    span.classList.remove('span-active');
+    }
 }
 
 inputs.forEach((input)=> input.addEventListener('focus',handlefocus));
+inputs.forEach((input)=> input.addEventListener('focusout',handlefocusout));
